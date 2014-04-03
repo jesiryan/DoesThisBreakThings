de520af87fde2fe45a83e491c1c67e58dc9db0b2
brendan <brendan>
2014-04-03 22:52:34 +0100

    started to read excel file

--- cd-html5-mobile/src/main/java/com/conygre/training/fileimport/FileReader.java
+++ cd-html5-mobile/src/main/java/com/conygre/training/fileimport/FileReader.java
@@ -3,6 +3,7 @@ package com.conygre.training.fileimport;
 import com.conygre.training.entities.*;
 import com.conygre.training.dao.jpa.PersistenceUtil;
 //import org.dt340a.group6.sprint1.persistence.test.*;
+import com.conygre.training.services.FileUploadServlet;
 import com.conygre.training.validation.ValidateExcelFile;
 
 import java.io.File;
@@ -37,7 +38,7 @@ public class FileReader {
 	}
 
 	public FileReader() {
-		this(PersistenceUtil.filePath);
+		this(FileUploadServlet.pathAndName);
 	}
 
 
