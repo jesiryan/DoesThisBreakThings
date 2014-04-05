905f783e238dd5bdcbb21409259d902126d9755c
brendan <brendan>
2014-04-05 12:45:48 +0100

    Story 13 web page receiving some information now

--- cd-html5-mobile/src/main/webapp/js/us13.js
+++ cd-html5-mobile/src/main/webapp/js/us13.js
@@ -1,21 +1,21 @@
-//****************************User Story 11*******************************//
+//****************************User Story 13*******************************//
     
-	/*Pass start and end dateTimes into this function which is called from the submit when you enter start and end dateTimes*/
-    function getUS11ResultTemplate(startdate,enddate) {
+	/*This function is called from the submit*/
+    function getUS13ResultTemplate() {
     	
         $.ajax({
-            url: "tmpl/us11.tmpl",
+            url: "tmpl/us13.tmpl",
             dataType: "html",
             success: function( data ) {
             	
                 $( "head" ).append( data );
-                updateUS11Table(startdate,enddate);
+                updateUS13Table();
             }
         });
     }
     
-    /* Uses JAX-RS GET to retrieve all imsi Numbers for start and end dateTimes*/
-    function updateUS11Table(startdate,enddate) {
+    /* Uses JAX-RS GET to retrieve all country, operator, cell combinations*/
+    function updateUS13Table() {
 
         $.ajax({
             url: "rest/net/us13/",
@@ -25,13 +25,13 @@
             	if (data.length < 1) {
                     $('#info').removeClass("hidden");
                     $('#hidden-container').addClass("hidden");
-            		$('#info').empty().append("Information: The query between dates " + startdate + " and " + enddate + " has returned no results.");
-					document.forms["us11Form"].reset();
+            		$('#info').empty().append("Information: The query has returned no results.");
+					document.forms["us13Form"].reset();
             	} else {
             		$('#info').addClass("hidden");
                     $('#hidden-container').removeClass("hidden");
-                    $('#results').empty().append(buildUS11ResultsRows(data));
-                    document.forms["us11Form"].reset();
+                    $('#results').empty().append(buildUS13ResultsRows(data));
+                    document.forms["us13Form"].reset();
                     console.log("The data being added is : " + data[0] + data[1] + data[2] + data[3]+ data[4] + data[5]);
                 }
             },
@@ -43,6 +43,6 @@
     }
 
 	/* Builds the updated table for the callfailures list */
-	function buildUS11ResultsRows(obj) {
-	    return _.template( $( "#us11-tmpl" ).html(), {"obj": obj});
+	function buildUS13ResultsRows(obj) {
+	    return _.template( $( "#us13-tmpl" ).html(), {"obj": obj});
 	}
